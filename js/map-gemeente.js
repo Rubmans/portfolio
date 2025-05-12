585484b7d33c9ba4428b1b429ba542ddaf0ab0ce
Ruben Droeshoudt <ruben.droeshoudt@student.ap.be>
2025-05-12 10:08:19 +0200

    aanpassing aanduiding map

--- js/map-gemeente.js
+++ js/map-gemeente.js
@@ -5,12 +5,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// bepaal de rechthoek rondom een gebied in Kontich (voorbeeldcoördinaten)
-let bounds = [[51.1330, 4.4470], [51.1315, 4.4500]];
-
-// kleur de rechthoek in met de rode AP-kleur (#e60005)
-L.rectangle(bounds, { color: "#e60005", weight: 1 }).addTo(map);
-
 // plaats een marker (coördinaten 51.1324, 4.4485) met als tekst "Kontich Centrum"
 let kontichMarker = L.marker([51.1324, 4.4485]).addTo(map);
 kontichMarker.bindPopup("<b>Kontich Centrum</b><br>Marktplein").openPopup();
+
+//bepaal de gemeentegrezen van Kontich
+fetch('kontich.geojson')
+  .then(response => response.json())
+  .then(geojsonData => {
+    L.geoJSON(geojsonData, {
+      style: {
+        color: "#e60005",
+        weight: 2,
+        fill: false
+      }
+    }).addTo(map);
+  });
